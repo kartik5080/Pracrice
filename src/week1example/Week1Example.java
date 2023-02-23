07f36bc5f83ab4d625492cd45c7b37d429ae1a62
kartik5080 <pahwakar@sheridancollege.ca>
2023-02-22 22:33:17 -0500

    commit 1

---
+++ src/week1example/Week1Example.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week1example;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class Week1Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // System.out.println("hello");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the word");
+        String word = sc.next();
+        char[] a= new char[word.length()];
+        a = word.toCharArray();
+        for(int i=a.length-1;i>=0;i--)
+        {
+            System.out.println(a[i]);
+        }
+        
+    }
+    
+}
